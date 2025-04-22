6defc38abbf2837e4566713608716e9ddf96b68d
Mialbo <catalogador1@gmail.com>
2025-04-21 21:33:45 -0500

    Delete usuarios-servicio/usuarios-servicio directory

--- usuarios-servicio/usuarios-servicio/src/main/java/com/sistemaeducativo/usuarios/repositorio/UsuarioRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.sistemaeducativo.usuarios.repositorio;
-
-import com.sistemaeducativo.usuarios.modelo.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
-    // Métodos personalizados si los necesitas más adelante
-}
